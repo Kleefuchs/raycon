33f6ed51afc9e9c907caa0eac7184fab7db9c298
Kleefuchs <92873182+Kleefuchs@users.noreply.github.com>
2024-07-23 20:16:15 +0200

    Got some basic shit working. Still need to work on input

--- src/app.rs
+++ src/app.rs
@@ -1,9 +1,11 @@
 use raylib::prelude::*;
 
 
-
 pub struct App {
-    pub rd: (RaylibHandle, RaylibThread),
+    rd: (RaylibHandle, RaylibThread),
+    keyboard: crate::keyboard::Keyboard,
+    input_handler: crate::input_handler::InputHandler,
+    output_handler: crate::output_handler::OutputHandler,
 }
 
 impl App {
@@ -13,17 +15,25 @@ impl App {
                 .size(1280, 720)
                 .title("raycon")
                 .build(),
+            keyboard: crate::keyboard::Keyboard::new(),
+            input_handler: crate::input_handler::InputHandler::new(),
+            output_handler: crate::output_handler::OutputHandler::new(),
         };
         return app;
     }
     fn init(&mut self) {
+        self.rd.0.set_target_fps(60);
         return;
     }
     fn update(&mut self) {
+        let input:String = self.keyboard.get(&mut self.rd.0);
+        let info:Vec<String> = self.input_handler.process(&input);
+        let mut output = self.output_handler.process(&info);
+        dbg!(info);
         return;
     }
     fn draw(&mut self) {
-        let mut draw = self.rd.0.begin_drawing(&self.rd.1);
+        let mut draw:RaylibDrawHandle = self.rd.0.begin_drawing(&self.rd.1);
         draw.clear_background(Color::BLACK);
         return;
     }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 mod app;
+mod keyboard;
+mod input_handler;
+mod output_handler;
 
 fn main() {
     let mut app:app::App = app::App::new();
